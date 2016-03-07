d6bf7df09620a76c73d8e00822a8f946ab2abc6d
zh20102618 <18146615950@163.com>
2016-03-07 16:31:40 +0800

    添加基本工程便于测试

---
+++ ZHVender/ZHVender/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZHVender
+//
+//  Created by 张晗 on 16/3/7.
+//  Copyright © 2016年 kuangxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZHVender/ZHVender/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZHVender
+//
+//  Created by 张晗 on 16/3/7.
+//  Copyright © 2016年 kuangxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
